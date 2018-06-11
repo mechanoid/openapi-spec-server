9cbdf0671f51ec1ecfbeab7995980037d0b7d269
Falk Hoppe <falkhoppe81@gmail.com>
2018-06-11 16:21:29 +0200

    add basic auth capabilities

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const basicAuth = require('basic-auth-connect')
 const swaggerUiAssetPath = require('swagger-ui-dist').getAbsoluteFSPath()
 
 module.exports = config => {
@@ -12,6 +13,10 @@ module.exports = config => {
   app.set('views', path.resolve(__dirname, 'views'))
   app.use(helmet())
   app.use(morgan('combined'))
+
+  if (process.env.OPENAPI_SPEC_SERVER_USER && process.env.OPENAPI_SPEC_SERVER_PASSWORD) {
+    app.use(basicAuth(process.env.OPENAPI_SPEC_SERVER_USER, process.env.OPENAPI_SPEC_SERVER_PASSWORD))
+  }
 
   app.use('/assets', express.static('./assets'))
   app.use('/vendor', express.static('./node_modules'))
